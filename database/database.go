65154bcc348d88a92405e3f5ecf5ce2c04aa2e60
Miftah <muhammadmiftahrizki@gmail.com>
2024-04-06 21:10:41 +0700

    changed the db import mechanism to fail the program if the db is nil

--- database/database.go
+++ database/database.go
@@ -2,22 +2,23 @@ package database
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func NewDB() (*sql.DB, error) {
+func New() *sql.DB {
 	db, err := sql.Open("mysql", "root:toor@tcp(localhost:3306)/portfolio?parseTime=true")
 	if err != nil {
-		return nil, err
+		log.Fatalln(err)
 	}
 
 	err = db.Ping()
 	if err != nil {
-		return nil, err
+		log.Fatalln(err)
 	}
 
 	db.SetMaxOpenConns(10)
 
-	return db, nil
+	return db
 }
