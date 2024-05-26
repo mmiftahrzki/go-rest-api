ea4456ea2bc7fae173052e08254305187d739f1b
Miftah <muhammadmiftahrizki@gmail.com>
2024-05-26 23:06:16 +0700

    changed databsase instance to singleton

--- database/database.go
+++ database/database.go
@@ -2,13 +2,21 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"sync"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func New() *sql.DB {
-	db, err := sql.Open("mysql", "root:toor@tcp(localhost:3306)/portfolio?parseTime=true")
+var once sync.Once
+var database_connection *sql.DB
+
+func new() *sql.DB {
+	var err error
+	var db *sql.DB
+
+	db, err = sql.Open("mysql", "root:toor@tcp(localhost:3306)/portfolio?parseTime=true")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -21,4 +29,18 @@ func New() *sql.DB {
 	db.SetMaxOpenConns(10)
 
 	return db
+}
+
+func GetDatabaseConnection() *sql.DB {
+	if database_connection != nil {
+		fmt.Println("koneksi database sudah pernah dibuat.")
+
+		return database_connection
+	}
+
+	once.Do(func() {
+		database_connection = new()
+	})
+
+	return database_connection
 }
