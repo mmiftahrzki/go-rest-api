8595d06fd9546112d987a76bfd6ba8aeff181910
Rizki <rizki@ignitech.net>
2024-07-20 22:48:53 +0700

    Add /api-docs endpoint that is composed with /swagger_css and /swagger_js endpoint for serving static HTML file swagger page.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"fmt"
 	"log"
 	"net/http"
@@ -19,6 +20,15 @@ import (
 	router_pkg "github.com/mmiftahrzki/go-rest-api/router"
 )
 
+//go:embed docs/swagger-ui.html
+var swagger_ui_html []byte
+
+//go:embed swagger.yaml
+var swagger_yaml []byte
+
+//go:embed static/*
+var static_fs embed.FS
+
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -31,6 +41,7 @@ func main() {
 	model_customer := model.NewCustomer(db, "customer")
 	controller_customer := controller.NewCustomer(model_customer)
 	router := router_pkg.New()
+
 	auth := auth_pkg.New()
 	customerValidation := validation.New()
 
@@ -46,6 +57,7 @@ func main() {
 	getCustomerById := router_pkg.Endpoint{Path: "/api/customers/:id", Method: http.MethodGet, Middlewares: []middleware.Middleware{auth}}
 	updateCustomer := router_pkg.Endpoint{Path: "/api/customers/:id", Method: http.MethodPut, Middlewares: []middleware.Middleware{auth}}
 	deleteCustomer := router_pkg.Endpoint{Path: "/api/customers/:id", Method: http.MethodDelete, Middlewares: []middleware.Middleware{auth}}
+	documentation := router_pkg.Endpoint{Path: "/api-docs", Method: http.MethodGet}
 
 	router.AddRoute(helloWorld, func(writer http.ResponseWriter, request *http.Request, parameters httprouter.Params) {
 		writer.Header().Set("Content-Type", "text/html")
@@ -54,9 +66,7 @@ func main() {
 	})
 	router.AddRoute(signUp, handler.CreateUser)
 	router.AddRoute(signIn, handler.ReadUser)
-
 	router.AddRoute(getToken, auth_pkg.Token)
-
 	router.AddRoute(createCustomer, controller_customer.Create)
 	router.AddRoute(getAllCustomers, controller_customer.ReadAll)
 	router.AddRoute(router_pkg.Endpoint{Path: "/api/customers/:id/next", Method: http.MethodGet}, controller_customer.ReadNext)
@@ -64,6 +74,42 @@ func main() {
 	router.AddRoute(updateCustomer, controller_customer.UpdateById)
 	router.AddRoute(deleteCustomer, controller_customer.Delete)
 	router.AddRoute(getCustomerById, controller_customer.ReadById)
+	router.AddRoute(router_pkg.Endpoint{Path: "/swagger-css", Method: http.MethodGet}, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Content-Type", "text/css")
+		w.WriteHeader(http.StatusOK)
+
+		const file_name = "static/swagger-ui.css"
+
+		file_content, err := static_fs.ReadFile(file_name)
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write([]byte(file_content))
+	})
+	router.AddRoute(router_pkg.Endpoint{Path: "/swagger-js", Method: http.MethodGet}, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Content-Type", "text/javascript")
+		w.WriteHeader(http.StatusOK)
+
+		const file_name = "static/swagger-ui-bundle.js"
+
+		file_content, err := static_fs.ReadFile(file_name)
+		if err != nil {
+			panic(err)
+		}
+
+		w.Write([]byte(file_content))
+	})
+	router.AddRoute(documentation, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+		w.Write(swagger_ui_html)
+	})
+	router.AddRoute(router_pkg.Endpoint{Path: "/swagger", Method: http.MethodGet}, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		w.Header().Set("Content-Type", "text/yaml")
+		w.WriteHeader(http.StatusOK)
+		w.Write(swagger_yaml)
+	})
 
 	server := http.Server{
 		Addr:    os.Getenv("BASE_URL") + ":" + os.Getenv("PORT"),
